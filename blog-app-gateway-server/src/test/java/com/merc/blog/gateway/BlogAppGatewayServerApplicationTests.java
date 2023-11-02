ab97fac16cebccad41d76b9826808df606df9dc9
Rachitha-Krishne <147464063+Rachitha-Krishne@users.noreply.github.com>
2023-11-02 16:21:08 +0530

    Update BlogAppGatewayServerApplicationTests.java

--- blog-app-gateway-server/src/test/java/com/merc/blog/gateway/BlogAppGatewayServerApplicationTests.java
+++ blog-app-gateway-server/src/test/java/com/merc/blog/gateway/BlogAppGatewayServerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.merc.blog.gateway;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppGatewayServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
